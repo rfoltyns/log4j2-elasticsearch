982d6dbbc8500f6df2ea9094238ad3c40c050be4
rfoltyns <r.foltynski@gmail.com>
2020-03-22 22:31:22 +0000

    ElementType attributes fixed

--- log4j2-elasticsearch-hc/src/main/java/org/appenders/log4j2/elasticsearch/hc/backoff/Log4j2BatchLimitBackoffPolicy.java
+++ log4j2-elasticsearch-hc/src/main/java/org/appenders/log4j2/elasticsearch/hc/backoff/Log4j2BatchLimitBackoffPolicy.java
@@ -20,16 +20,16 @@ package org.appenders.log4j2.elasticsearch.hc.backoff;
  * #L%
  */
 
-import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.config.ConfigurationException;
 import org.apache.logging.log4j.core.config.Node;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginBuilderAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginBuilderFactory;
+import org.appenders.log4j2.elasticsearch.backoff.BackoffPolicy;
 import org.appenders.log4j2.elasticsearch.backoff.BatchLimitBackoffPolicy;
 import org.appenders.log4j2.elasticsearch.hc.BatchRequest;
 
-@Plugin(name = Log4j2BatchLimitBackoffPolicy.PLUGIN_NAME, category = Node.CATEGORY, elementType = Appender.ELEMENT_TYPE, printObject = true)
+@Plugin(name = Log4j2BatchLimitBackoffPolicy.PLUGIN_NAME, category = Node.CATEGORY, elementType = BackoffPolicy.NAME, printObject = true)
 public class Log4j2BatchLimitBackoffPolicy extends BatchLimitBackoffPolicy<BatchRequest> {
 
     static final String PLUGIN_NAME = "BatchLimitBackoffPolicy";
