be7fd3e482cc15c2c6fa05417819c0e197a84208
rfoltyns <r.foltynski@gmail.com>
2020-10-28 21:41:28 +0000

    Basic ECS (Elastic Common Schema) support (#55)

---
+++ log4j2-elasticsearch-core/src/main/java/org/appenders/log4j2/elasticsearch/ecs/ThrowableProxyStackTraceAsStringEcsSerializer.java
@@ -0,0 +1,40 @@
+package org.appenders.log4j2.elasticsearch.ecs;
+
+/*-
+ * #%L
+ * log4j2-elasticsearch
+ * %%
+ * Copyright (C) 2020 Rafal Foltynski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.apache.logging.log4j.core.impl.ThrowableProxy;
+
+import java.io.IOException;
+
+public class ThrowableProxyStackTraceAsStringEcsSerializer extends JsonSerializer<ThrowableProxy> {
+
+    @Override
+    public void serialize(ThrowableProxy value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+
+        // Let's start small
+        gen.writeString(value.getExtendedStackTraceAsString());
+
+    }
+
+}
