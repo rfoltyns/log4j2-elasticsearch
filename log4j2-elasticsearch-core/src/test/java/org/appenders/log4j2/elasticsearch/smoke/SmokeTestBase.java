7b19186de1b9e5003d4694feb03ea58bcf6efed8
rfoltyns <r.foltynski@gmail.com>
2022-10-12 23:39:32 +0100

    Refactor SmokeTest load generator

--- log4j2-elasticsearch-core/src/test/java/org/appenders/log4j2/elasticsearch/smoke/SmokeTestBase.java
+++ log4j2-elasticsearch-core/src/test/java/org/appenders/log4j2/elasticsearch/smoke/SmokeTestBase.java
@@ -38,9 +38,11 @@ import org.appenders.log4j2.elasticsearch.failover.Log4j2SingleKeySequenceSelect
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Queue;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 import java.util.function.Function;
@@ -53,7 +55,8 @@ import static org.appenders.log4j2.elasticsearch.failover.ChronicleMapUtil.resol
 
 public abstract class SmokeTestBase {
 
-    public static final long ONE_SECOND = TimeUnit.MILLISECONDS.toNanos(1000);
+    public static final long ONE_SECOND_MILLIS = 1000;
+    public static final long ONE_SECOND_NANOS = TimeUnit.MILLISECONDS.toNanos(ONE_SECOND_MILLIS);
 
     final TestConfig config = new TestConfig();
 
@@ -75,8 +78,8 @@ public abstract class SmokeTestBase {
             .add("logSizeInBytes", getInt("smokeTest.logSizeInBytes", 1))
             .add("lifecycleStopDelayMillis", getInt("smokeTest.lifecycleStopDelayMillis", 10000))
             .add("exitDelayMillis", getInt("smokeTest.exitDelayMillis", 10000))
-            .add("numberOfProducers", getInt("smokeTest.noOfProducers", 5))
-            .add("producerBatchSize", getInt("smokeTest.producerBatchSize", 10))
+            .add("numberOfProducers", getInt("smokeTest.noOfProducers", 10))
+            .add("producerBatchSize", getInt("smokeTest.producerBatchSize", 100))
             .add("producerSleepMillis", getInt("smokeTest.initialProducerSleepMillis", 20))
             .add("loggerName", System.getProperty("smokeTest.loggerName", "elasticsearch-logger"))
             .add("appenderName", System.getProperty("smokeTest.appenderName", "elasticsearch-appender"))
@@ -228,48 +231,59 @@ public abstract class SmokeTestBase {
 
     <T> void indexLogs(Logger logger, Marker marker, int numberOfProducers, Supplier<T> logSupplier) throws InterruptedException {
 
-        final AtomicInteger limitTotal = new AtomicInteger(getConfig().getProperty("limitTotal", Integer.class));
-        final AtomicInteger producerSleepMillis = new AtomicInteger(getConfig().getProperty("producerSleepMillis", Integer.class));
-        final AtomicInteger producerBatchSize = new AtomicInteger(getConfig().getProperty("producerBatchSize", Integer.class));
+        final AtomicInteger itemsLeft = new AtomicInteger(getConfig().getProperty("limitTotal", Integer.class));
+        int itemsExpected = itemsLeft.get();
+        final int producerSleepMillis = getConfig().getProperty("producerSleepMillis", Integer.class);
+        final int producerBatchSize = getConfig().getProperty("producerBatchSize", Integer.class);
         final int limitPerSec = getConfig().getProperty("limitPerSec", Integer.class);
 
-        CountDownLatch latch = new CountDownLatch(numberOfProducers);
-
-        int numberOfLogsToDeliver = limitTotal.get();
-        for (int thIndex = 0; thIndex < numberOfProducers; thIndex++) {
-            new Thread(() -> {
-                while (limitTotal.get() >= 0) {
-                    logMicroBatch(producerBatchSize.get(), limitTotal, logger, marker, logSupplier);
-                    LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(producerSleepMillis.get()));
-                }
-                latch.countDown();
-            }).start();
-        }
-
-        while (latch.getCount() != 0) {
-
-            LockSupport.parkNanos(ONE_SECOND);
-
-            int count = localCounter.getAndSet(0);
-            int sleepMillis = producerSleepMillis.get();
-
-            if (count > limitPerSec && sleepMillis != 1) {
-                producerSleepMillis.incrementAndGet();
-            } else if (sleepMillis > 1) {
-                producerSleepMillis.decrementAndGet();
-            } else if (count < limitPerSec) {
-                producerBatchSize.incrementAndGet();
+        final Supplier<Boolean> shouldContinue = () -> itemsLeft.get() > 0;
+        final LoadTask loadTask = new LoadTask() {
+            public void generateLoad(final int size, final long sleepMillis) {
+                logMicroBatch(size, itemsLeft, logger, marker, logSupplier);
+                LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(sleepMillis));
             }
+        };
 
+        final Queue<ConditionalLoop> producers = new ConcurrentLinkedQueue<>();
+        final LoadGenerator loadGenerator = new LoadGenerator(producers, new LoadProducerFactory(loadTask, shouldContinue), new RampUpPolicy(limitPerSec, 0.1), numberOfProducers, producerSleepMillis, producerBatchSize);
+        final long projectedCountPerMillis = loadGenerator.projectedCountPerMillis(ONE_SECOND_MILLIS);
+        loadGenerator.reconfigure(projectedCountPerMillis / (double) limitPerSec, limitPerSec);
+
+        loadGenerator.start();
+
+        final AtomicInteger noProgressRecorded = new AtomicInteger();
+        while (shouldContinue.get()) {
+
+            LockSupport.parkNanos(ONE_SECOND_NANOS);
+
+            final int count = localCounter.getAndSet(0);
+
+            final double currentLoad = (double) count / (double) limitPerSec;
             String stats = String.format(
-                    "Sleep millis per producer: %d, Producer batch size: %d, Current throughput: %d/s; Progress: %d/%d",
-                    sleepMillis,
-                    producerBatchSize.get(),
+                    "Sleep millis: %d, Producers: %d, Micro batch size: %d, Throughput: %d/s; Test Load: %.3f, Progress: %d/%d",
+                    loadGenerator.getProducerSleepMillis(),
+                    loadGenerator.getNumberOfProducers(),
+                    loadGenerator.getProducerBatchSize(),
                     count,
+                    currentLoad,
                     totalCounter.get(),
-                    numberOfLogsToDeliver);
+                    itemsExpected);
 
             System.out.println(stats);
+
+            // On finished or stuck/broken
+            if (count == 0) {
+                noProgressRecorded.incrementAndGet();
+            }
+
+            // Fuse
+            if (noProgressRecorded.get() > 5) {
+                break;
+            }
+
+            loadGenerator.reconfigure(currentLoad, limitPerSec);
+
         }
 
         sleep(getConfig().getProperty("lifecycleStopDelayMillis", Integer.class));
@@ -287,6 +301,216 @@ public abstract class SmokeTestBase {
             localCounter.incrementAndGet();
             totalCounter.incrementAndGet();
         }
+    }
+
+    private static class LoadGenerator {
+
+        private final Queue<ConditionalLoop> producers;
+        private final LoadProducerFactory producerFactory;
+        private final AtomicInteger numberOfProducers;
+        private final AtomicInteger producerSleepMillis;
+        private final AtomicInteger producerBatchSize;
+        private final ThrottlingPolicy throttlingPolicy;
+
+        public LoadGenerator(final Queue<ConditionalLoop> initialProducers,
+                             final LoadProducerFactory producerFactory,
+                             final ThrottlingPolicy throttlingPolicy,
+                             final int numberOfProducers,
+                             final int producerSleepMillis,
+                             final int producerBatchSize) {
+            this.producers = initialProducers;
+            this.producerFactory = producerFactory;
+            this.throttlingPolicy = throttlingPolicy;
+            this.numberOfProducers = new AtomicInteger(numberOfProducers);
+            this.producerSleepMillis = new AtomicInteger(producerSleepMillis);
+            this.producerBatchSize = new AtomicInteger(producerBatchSize);
+        }
+
+        private int reconfigure(final double currentLoad, final int limitPerSec) {
+
+            if (currentLoad <= 0) {
+                return 0;
+            }
+
+            final int sleepMillis = producerSleepMillis.get();
+
+            double targetLoad = throttlingPolicy.throttle(currentLoad, limitPerSec);
+
+            final int newSleepMillis = (int) (sleepMillis * currentLoad);
+            producerSleepMillis.set(Math.max(newSleepMillis, 5)); // 5ms for now. That's 200 runs per second.
+
+            while (projectedLoad(limitPerSec) > 1.05d) {
+                producerSleepMillis.incrementAndGet();
+            }
+
+            if (projectedLoad(limitPerSec) < 1.0) {
+
+                final int batchSize = producerBatchSize.get();
+                final int producers = numberOfProducers.get();
+
+                if (batchSize != 100) { // 100 logs per call at this point is 20000/sec per producer at targetLoad == 1
+                    final int newBatchSize = (int) (batchSize / targetLoad);
+                    producerBatchSize.set(Math.min(newBatchSize, 100));
+                } else if (producers < 32) {
+
+                    long projectedRate = projectedCountPerMillis(1000);
+
+                    final int moreProducers = (int) (limitPerSec / projectedRate);
+                    numberOfProducers.addAndGet(moreProducers);
+
+                    final int lessProducers = (int) (projectedRate / limitPerSec);
+                    numberOfProducers.addAndGet(-lessProducers);
+                }
+
+            }
+
+            while (producers.size() > numberOfProducers.get()) {
+                producers.remove().stopLoop();
+            }
+            while (producers.size() < numberOfProducers.get()) {
+                producers.add(createInternal());
+            }
+            if (currentLoad - projectedLoad(limitPerSec) > 0.05) {
+                System.out.println("Stopping over-producing loop");
+                producers.remove().stopLoop();
+            }
+
+            return numberOfProducers.get();
+
+        }
+
+        private double projectedLoad(int limitPerSec) {
+            return projectedCountPerMillis(1000) / (double) limitPerSec;
+        }
+
+        public long projectedCountPerMillis(long millis) {
+            return  (millis / producerSleepMillis.get()) * numberOfProducers.get() * producerBatchSize.get();
+        }
+
+        public int getNumberOfProducers() {
+            return producers.size();
+        }
+
+        public int getProducerBatchSize() {
+            return producerBatchSize.get();
+        }
+
+        public int getProducerSleepMillis() {
+            return producerSleepMillis.get();
+        }
+
+        public void start() {
+
+            while (producers.size() < getNumberOfProducers()) {
+                producers.add(createInternal());
+            }
+
+        }
+
+        private ConditionalLoop createInternal() {
+            return producerFactory.createProducer(true, this::getProducerBatchSize, this::getProducerSleepMillis);
+        }
+
+    }
+
+    private static class LoadProducerFactory {
+
+        private final LoadTask task;
+        private final Supplier<Boolean> condition;
+
+        private LoadProducerFactory(final LoadTask task, final Supplier<Boolean> condition) {
+            this.task = task;
+            this.condition = condition;
+        }
+
+        public ConditionalLoop createProducer(final boolean startOnCreate, final Supplier<Integer> sizeSupplier, final Supplier<Integer> sleepMillisSupplier) {
+
+            final Runnable generateLoad = () -> task.generateLoad(sizeSupplier.get(), sleepMillisSupplier.get());
+            final ConditionalLoop loop = new ConditionalLoop(generateLoad, condition);
+
+            if (startOnCreate) {
+                loop.start();
+            }
+
+            return loop;
+
+        }
+
+    }
+
+    private static class ConditionalLoop extends Thread {
+
+        private final Runnable runnable;
+        private final Supplier<Boolean> condition;
+        private final AtomicBoolean running = new AtomicBoolean(true);
+
+        public ConditionalLoop(final Runnable runnable, final Supplier<Boolean> condition) {
+            this.runnable = runnable;
+            this.condition = condition;
+        }
+
+        @Override
+        public void run() {
+
+            while (running.get() && condition.get()) {
+                runnable.run();
+            }
+            stopLoop();
+
+        }
+
+        public void stopLoop() {
+            running.compareAndSet(true, false);
+        }
+
+    }
+
+    private abstract static class LoadTask {
+
+        public LoadTask() {
+        }
+
+        public abstract void generateLoad(final int size, final long sleep);
+
+    }
+
+    private interface ThrottlingPolicy {
+        double throttle(final double currentLoad, final int limitPerSec);
+    }
+
+    private static class RampUpPolicy implements ThrottlingPolicy {
+
+        private final int max;
+        private final double percentOnResize;
+
+        public RampUpPolicy(final int max, final double resizeFactor) {
+
+            if (resizeFactor > 1) {
+                throw new IllegalArgumentException("Resize factor too aggressive for ramp up policy: " + resizeFactor);
+            }
+
+            this.max = max;
+            this.percentOnResize = max * resizeFactor / max;
+
+        }
+
+        @Override
+        public double throttle(final double currentLoad, final int limitPerSec) {
+
+            if (currentLoad == 0.0d) {
+                return currentLoad;
+            }
+
+            if (currentLoad * limitPerSec > max && currentLoad > 1.05d) {
+                return 1.0d + percentOnResize;
+            } else if (currentLoad < 0.95d){
+                return currentLoad + (currentLoad * percentOnResize);
+            } else {
+                return 1.0d;
+            }
+
+        }
+
     }
 
 }
